062b1944ba54d3c881c81b6616de62b26d96bae1
Atle Prange <atle.prange@gmail.com>
2013-08-27 09:02:48 +0200

    Oppdatert oppgavetekst

--- src/main/java/dhts/Tasks1.java
+++ src/main/java/dhts/Tasks1.java
@@ -22,60 +22,51 @@ public class Tasks1 {
 
         /*
         3)
-        Når man jobber med databaser er det vanlig å ha et id felt i objektene som lagres. Det er også vanlig å gjøre en Null-sjekk i dao laget for å sjekke om et objekt er nytt eller "gammelt"
+        Når man jobber med databaser er det vanlig å ha et id felt i objektene som lagres. Det er også vanlig å gjøre en Null-sjekk op id i dao laget for å sjekke om et objekt er nytt eller "gammelt"
         Når man lagrer et tidligere ulagret objekt får man ofte tilbake de samme objektet med id, eller bare en id tilbake.
 
         Nå skal vi fjerne denne tvedydigheten om et objekt er lagret eller ulagret:
+
         Lag en sort som represtenter et lagret objekt, og endre metodesignaturene til dao motodene i Books til å reflektere dette.
         Implementer en enkel lagring vha books HashMap<Object,Book> feltet i Books
-        */
-
-        /*
-
-        Bakgrunn:
-        Immutability
+        Hva skjer når man prøver å hente en bok som ikke finnes i lageret? Reflekterer metodesignaturene dette? (Hint, exception er ikke typesikkert)
 
 
-        4.a)
-        Hvis du ikke allerede har gjort det: Gjør Book immutable for å forenkle testing.
+        Du kan bruke bookListGenerator for å lage en liste med bøker for å teste.
 
-        4.b)
-        Lag en kodesnutt som lagrer bøker og henter dem fram igjen. (Hint, bruk bookListGenerator for å lage en liste med bøker)
-        Hvordan lager man id-er for å teste henting av bøker fra lager? Hva skjer når en bok ikke finnes i lageret? Reflekterer metodesignaturene dette? (Hint, exception er ikke typesikkert)
-
+        4)
+        Hva skjer når hvis flere tråder henter samme bok og gjør endringer på den?
+        Hva er forventet oppførsel når man gjør endringer? Må man lagre boka igjen for at de skal "sitte"?
 
          */
 
 
         /*
         Bakgrunn:
-        Command pattern, programmer, interpreter, map(), bind().
+
 
         5)
         Dao metodene eksponerer delt endrebar tilstand og Books endrer tilstand mellom hvert kall. Dette ønsker vi å unngå.
         Lag en eller flere sorter som represtenterer db-operasjoner og data uten at daoen faktisk endrer tilstand.
-        Endre dao-funksjoner for å reflektere dette.
+
          */
 
 
         /*
         6)
-        Man ønsker som regel å sette sammen flere databaseoperasjoner, men bare gjøre ett eneste kall til execute metoden sin.
-        Endre sortene slik at de kan settes sammen.
+        Lag en en ny versjon av Books som kjører databaseoperasjoner.
          */
 
 
-        /*
-        Bakgrunn:
-        fold()
-         */
+
 
         /*
         Sett det hele sammen:
 
-        Skriv en funksjon som skal sjekke isbn nummeret til en bok, og gjøre alle bokstaver i den til uppercase.
-          Skriv så et lite program som ved hjelp av en id laster inne en bok, endrer nummeret på den, lagrer den oppdaterte versjonen og returnerer en indikasjon på om operasjonen var vellykket eller ikke.
-          (Hint, hvis du kan: Lag programmet som en sort som du kan returnere, og som kan kjøres av en interpreter)
+        Lag først en funksjon som skal sjekke isbn nummeret til en bok, og gjøre alle bokstaver i den til uppercase.
+
+        Skriv så et lite program som ved hjelp av en id laster inne en bok, endrer nummeret på den, lagrer den oppdaterte versjonen og returnerer en indikasjon på om operasjonen var vellykket eller ikke.
+
 
          */
 
